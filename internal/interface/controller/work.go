12b694ce3334c545ed935bb4e57e5a72a884e668
Taku2515 <it.program.2357@gmail.com>
2025-09-02 16:02:45 +0900

    エラーハンドリングの改善

--- internal/interface/controller/work.go
+++ internal/interface/controller/work.go
@@ -1,6 +1,9 @@
 package controller
 
 import (
+	"database/sql"
+	"errors"
+
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/simesaba80/toybox-back/internal/interface/schema"
@@ -35,11 +38,14 @@ func (wc *WorkController) GetWorkByID(c echo.Context) error {
 	idStr := c.Param("work_id")
 	id, err := uuid.Parse(idStr)
 	if err != nil {
-		return err
+		return echo.NewHTTPError(400, "Invalid work ID format")
 	}
 
 	work, err := wc.workUsecase.GetByID(c.Request().Context(), id)
 	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return echo.NewHTTPError(404, "Work not found")
+		}
 		return err
 	}
 
