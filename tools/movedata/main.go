37cd966496ebb5c1aa0c88d68bf114fdeeb6cdcf
simesaba80 <simesaba80>
2025-09-14 13:17:03 +0900

    DB接続用にディレクトリ分割

--- tools/movedata/main.go
+++ tools/movedata/main.go
@@ -1,21 +1,12 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	"log"
-	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/simesaba80/toybox-back/internal/infrastructure/config"
 	"github.com/simesaba80/toybox-back/pkg/db"
-	"github.com/uptrace/bun"
-	"github.com/uptrace/bun/dialect/pgdialect"
-	"github.com/uptrace/bun/driver/pgdriver"
-	"github.com/uptrace/bun/extra/bundebug"
+	"github.com/simesaba80/toybox-back/tools/movedata/internal/connect"
 )
-
-var DB *bun.DB
 
 func main() {
 	// This is a placeholder main function.
@@ -26,31 +17,8 @@ func main() {
 	db.Init()
 	fmt.Println("Connected to the database1.")
 	//移行前のDB接続
-	err := godotenv.Load()
-	if err != nil {
-		log.Printf("読み込み出来ませんでした: %v", err)
-	}
-
-	DB_DSN_BACKUP := os.Getenv("DB_DSN_BACKUP")
-	sqlDB := sql.OpenDB(pgdriver.NewConnector(
-		pgdriver.WithDSN(DB_DSN_BACKUP),
-	))
-	DB = bun.NewDB(sqlDB, pgdialect.New())
-
-	if err := sqlDB.Ping(); err != nil {
-		log.Fatalf("Failed to connect to database: %v", err)
-		return
-	}
+	connect.Connect()
 	fmt.Println("Connected to the database2.")
-	// クエリーフックを追加
-	DB.AddQueryHook(bundebug.NewQueryHook(
-		bundebug.WithVerbose(true),
-	))
-
-	if err != nil {
-		log.Fatalf("Migration failed: %v", err)
-		return
-	}
 
 	fmt.Println("Migration script executed.")
 }
