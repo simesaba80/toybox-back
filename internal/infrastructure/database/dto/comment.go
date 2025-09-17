3cd58afe1603539ff6018a81436f32975c629fb0
Taku2515 <it.program.2357@gmail.com>
2025-09-17 18:00:31 +0900

    #16 DTOのcommentを修正

--- internal/infrastructure/database/dto/comment.go
+++ internal/infrastructure/database/dto/comment.go
@@ -15,7 +15,7 @@ type Comment struct {
 	Content       string           `json:"content" bun:"content,notnull"`
 	WorkID        uuid.UUID        `json:"work_id" bun:"work_id,notnull"`
 	UserID        uuid.UUID        `json:"user_id" bun:"user_id"`
-	ReplyAt       *uuid.UUID       `json:"reply_at" bun:"reply_at"`
+	ReplyAt       string           `json:"reply_at" bun:"reply_at"`
 	Visibility    types.Visibility `json:"visibility" bun:"visibility,notnull"`
 	CreatedAt     time.Time        `json:"created_at" bun:"created_at,notnull"`
 	UpdatedAt     time.Time        `json:"updated_at" bun:"updated_at,notnull"`
