96d247287e38d949d30339d913a74196d2b1f013
simesaba80 <simesaba1120@gmail.com>
2025-12-08 21:27:06 +0900

    [feat] #69 mypage用APIのユースケースとリポジトリ実装

--- internal/usecase/user.go
+++ internal/usecase/user.go
@@ -2,8 +2,9 @@ package usecase
 
 import (
 	"context"
-	"time"
+	"fmt"
 
+	"github.com/google/uuid"
 	"github.com/simesaba80/toybox-back/internal/domain/entity"
 	"github.com/simesaba80/toybox-back/internal/domain/repository"
 )
@@ -13,17 +14,23 @@ type IUserUseCase interface {
 }
 
 type userUseCase struct {
-	repo    repository.UserRepository
-	timeout time.Duration
+	repo repository.UserRepository
 }
 
-func NewUserUseCase(repo repository.UserRepository, timeout time.Duration) IUserUseCase {
+func NewUserUseCase(repo repository.UserRepository) IUserUseCase {
 	return &userUseCase{
-		repo:    repo,
-		timeout: time.Second * 30,
+		repo: repo,
 	}
 }
 
 func (u *userUseCase) GetAllUser(ctx context.Context) ([]*entity.User, error) {
 	return u.repo.GetAll(ctx)
+}
+
+func (u *userUseCase) GetByUserID(ctx context.Context, id uuid.UUID) (*entity.User, error) {
+	user, err := u.repo.GetByID(ctx, id)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get user by ID %s: %w", id.String(), err)
+	}
+	return user, nil
 }
